71722437902ce7b025651e0c46175880b6bdfa95
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-27 14:06:43 +0200

    done

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ def increment_counter():
 # Ihr Ziel ist es, diesen Code zu refaktorieren,
 # um die globale Variable durch ein Closure (create_counter) zu ersetzen.
 def create_counter():
+    # TODO: Implementieren Sie hier die Funktion create_counter
     ...
 
 
 if __name__ == '__main__':
-    # Auch mit dem Closure soll der Aufruf von increment_counter() wie gewohnt funktionieren.
+    # Auch mit dem Closure soll der Aufruf von increment_counter()
+    # wie gewohnt funktionieren.
+    # TODO: Erstellen Sie hier das Closure.
     increment_counter()
     increment_counter()
     increment_counter()
