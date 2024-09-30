35084d32ba52ae4c551661330186c88b704290c5
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:07:28 +0200

    Ran pyink on branch main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # Version mit globaler Variable
 counter = 0
 
+
 def increment_counter():
     global counter
     counter += 1
     print(f"Counter: {counter}")
     return counter
+
 
 # Ihr Ziel ist es, diesen Code zu refaktorieren,
 # um die globale Variable durch ein Closure (create_counter) zu ersetzen.
@@ -14,7 +16,7 @@ def create_counter():
     ...
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Auch mit dem Closure soll der Aufruf von increment_counter()
     # wie gewohnt funktionieren.
     # TODO: Erstellen Sie hier das Closure.
